baa16ab0843fb9fbbc759ce08cc3f0fe60bb98a5
voldanila <73543284+voldanila@users.noreply.github.com>
2020-11-04 12:06:44 +0300

    Добавьте файлы проекта.

---
+++ ConditionOperators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string adminPassword = "123";
+            const string adminLogin = "admin";
+
+            const string guestPassword = "555";
+            const string guestLogin = "guest";
+
+            Console.WriteLine("Введите логин");
+            string userLogin = Console.ReadLine();
+
+            Console.WriteLine("Введите пароль");
+            string userPassword = Console.ReadLine();
+
+            //переделать условие чтобы выводилось Добро пожаловать админ или гость (идентифицировал)
+            if (userLogin == adminLogin && adminPassword == userPassword ||
+                userLogin == guestLogin && guestPassword == userPassword)
+            {
+                Console.WriteLine("Добро пожаловать");
+            }
+            else
+            {
+                Console.WriteLine("Неверный логин или пароль");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WelcomeSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текущее время");
+            int hours = int.Parse(Console.ReadLine());
+
+            if (hours >= 5 && hours < 12)
+            {
+                Console.WriteLine("Доброе утро");
+            }
+            else if (hours >= 12 && hours < 17)
+            {
+                Console.WriteLine("Добрый день");
+            }
+            else if (hours >= 17 && hours < 24)
+            {
+                Console.WriteLine("Добрый вечер");
+            }
+            else if (hours >= 0 && hours < 5)
+            {
+                Console.WriteLine("Доброй ночи");
+            }
+            else
+            {
+                Console.WriteLine("Некорректное время");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
